ee1743c41a2ffc1cbdcb8d7bd6894242eab545f9
Siya <siya123jc@gmail.com>
2024-09-16 10:40:08 +0530

    Updated

--- vectors/linearSearch.cpp
+++ vectors/linearSearch.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
+int input;
+vector<int> v;
+vector<int> Inputele(){
     int ele , input = 0 , stop = 0;
-    vector <int> v;
     while(stop == 0){
         cout << "Enter 1 to stop entering elements : ";
         cin >> input;
@@ -18,11 +19,19 @@ int main(){
             }
         }
     }
-    cout << "Enter the element u r searching for :";
-    cin >> input;
-    for(int ele: v) if(ele == input) cout << "Element found !!!!" << endl; //Linear Search
+    return v;
+}
+void display(vector <int> &v){
     cout << "The vector is : "<< endl;
     for(int val: v) cout << val << "\t";
     cout << endl;
+}
+int main(){
+    v = Inputele();
+    display(v);
+    cout << "Enter the element u r searching for :";
+    cin >> input;
+    for(int ele: v) if(ele == input) cout << "Element found !!!!" << endl; //Linear Search
+    display(v);
     return 0;
 }
